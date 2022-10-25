57d818ace562ca34ddd114d15ed084a0d405f046
apple <welwitschia_@naver.com>
2022-10-25 16:41:06 +0900

    plus matplot

--- day06/bookCrawling.py
+++ day06/bookCrawling.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pandas as pd
+import re
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 path = 'C:\\python\\chromedriver_win32\\chromedriver.exe'
 options = wd.ChromeOptions()
@@ -38,8 +41,41 @@ for book in bestSellerList:
     except:
         continue
 
-# print(datas)
-
 book_df = pd.DataFrame(datas, columns=('title', 'price', 'score'))
-book_df.to_csv('day06/bestseller.csv', mode='w',
+book_df.to_csv('result.csv', mode='w',
                encoding='utf-8-sig', index=True)
+
+bestseller_dict = {
+    '12,000원 이상': 0,
+    '13,000원 이상': 0,
+    '14,000원 이상': 0,
+    '15,000원 이상': 0,
+    '16,000원 이상': 0
+}
+
+for item in datas:
+    price = item[1]
+    priceNew = float(re.sub(',', '', price))
+
+    if priceNew >= 16000:
+        bestseller_dict['16,000원 이상'] += 1
+    elif priceNew >= 15000:
+        bestseller_dict['15,000원 이상'] += 1
+    elif priceNew >= 14000:
+        bestseller_dict['14,000원 이상'] += 1
+    elif priceNew >= 13000:
+        bestseller_dict['13,000원 이상'] += 1
+    elif priceNew >= 12000:
+        bestseller_dict['12,000원 이상'] += 1
+
+print(bestseller_dict)
+
+
+font_name = mpl.font_manager.FontProperties(
+    fname='c:/Windows/fonts/malgun.ttf').get_name()
+mpl.rc('font', family=font_name)
+
+figure = plt.figure()
+axes = figure.add_subplot(111)
+axes.pie(bestseller_dict.values(), labels=bestseller_dict.keys(), autopct='%.1f%%')
+plt.show()
